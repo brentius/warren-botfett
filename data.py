ca559647decf39153fc4f5066555e5b75a71a9fe
brentius <brentyee28@gmail.com>
2025-06-21 17:26:10 +0100

    reformatted data.py

--- data.py
+++ data.py
@@ -9,9 +9,7 @@ from dotenv import load_dotenv
 import os
 
 #load api key
-
 load_dotenv()
-
 alpaca_api_key = os.getenv("APCA_API_KEY_ID")
 alpaca_secret = os.getenv("APCA_API_SECRET_KEY")
 base_url = os.getenv("APCA_API_BASE_URL")
@@ -19,3 +17,4 @@ base_url = os.getenv("APCA_API_BASE_URL")
 #REST client
 from alpaca_trade_api import REST
 api = REST(alpaca_api_key, alpaca_secret, base_url)
+
