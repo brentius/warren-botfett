5f03b08d19e9bc057adb53b21a68fd53326bbb9d
brentius <brentyee28@gmail.com>
2025-06-21 16:53:58 +0100

    testing git v3

---
+++ data.py
@@ -0,0 +1,21 @@
+#Data.py
+#Job: Get and clean price data
+
+#imports
+import pandas
+import yfinance
+import alpaca_trade_api
+from dotenv import load_dotenv
+import os
+
+#load api key
+
+load_dotenv()
+
+alpaca_api_key = os.getenv("APCA_API_KEY_ID")
+alpaca_secret = os.getenv("APCA_API_SECRET_KEY")
+base_url = os.getenv("APCA_API_BASE_URL")
+
+#REST client
+from alpaca_trade_api import REST
+api = REST(alpaca_api_key, alpaca_secret, base_url)
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-##testing
+#gitesting
