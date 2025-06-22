3bd9793af59defc062085f19e058eaf763d9598e
brentius <brentyee28@gmail.com>
2025-06-22 17:36:00 +0100

    referenced nested function (data.py)

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 #main.py
 #puts everything together
 
-from data import historical_fetch, clean_data
+from data import historical_fetch
 from strategy import evaluate
 
 #define symbols, fetch + clean historical data
 symbols = ["AAPL", "MSFT", "NVDA", "TSLA"]
-raw_history = historical_fetch(symbols, start="2025-01-01")
-history_data = clean_data(raw_history, symbols)
+history_data = historical_fetch(symbols, start="2025-01-01")
 
 #evaluate signals based on strategy - BUY / HOLD / SELL
 signals = evaluate(history_data)
