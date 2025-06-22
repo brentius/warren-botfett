58cd5366ee6cadf84f8d67165fa88e2b3532641a
brentius <brentyee28@gmail.com>
2025-06-22 10:46:05 +0100

    separated clean_data and historical_fetch

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ import alpaca_trade_api
 from dotenv import load_dotenv
 import os
 
-#load api key
+#load api keys
 load_dotenv()
 alpaca_api_key = os.getenv("APCA_API_KEY_ID")
 alpaca_secret = os.getenv("APCA_API_SECRET_KEY")
@@ -20,16 +20,19 @@ api = REST(alpaca_api_key, alpaca_secret, base_url)
 
 #fetch historical bars + clean
 symbols = ["AAPL", "MSFT", "NVDA", "TSLA"]
+
+def clean_data(raw_history, symbols):
+    history_data = {}
+    for symbol in symbols:
+        df = raw_history.loc[symbol].copy()
+        df.index = pd.to_datetime(df.index)
+        df.index = df.index.tz_localize("UTC").tz_convert("America/New York")
+        df.sort_index(inplace = True)
+        df.columns = [col.lower() for col in df.columns]
+        df.dropna(inplace = True)
+        history_data[symbol] = df
+    return history_data
+
 def historical_fetch(symbols, timeframe = "1Day", start = "2025-01-01"):
     raw_history = api.get_bars(symbols, timeframe, start = start, adjustment = "raw").df
-    def clean_data(raw_history, symbols):
-        history_data = {}
-        for symbol in symbols:
-            df = raw_history.loc[symbol].copy()
-            df.index = pd.datetime(df.index)
-            df.index = df.index.tz_convert("America/New York")
-            df.sort_index(inplace = True)
-            df.columns = [col.lower() for col in df.columns]
-            df.dropna(inplace = True)
-            history_data[symbol] = df
     return clean_data(raw_history, symbols)
