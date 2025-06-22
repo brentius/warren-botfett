8ceb9dce3c815f0e05054c87fa7966695e44018b
brentius <brentyee28@gmail.com>
2025-06-22 17:27:45 +0100

    added cleanup of data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #main.py
 #puts everything together
 
-from data import historical_fetch
+from data import historical_fetch, clean_data
 from strategy import evaluate
 
 symbols = ["AAPL", "MSFT", "NVDA", "TSLA"]
-history_data = historical_fetch(symbols, start="2025-01-01")
+raw_history = historical_fetch(symbols, start="2025-01-01")
+history_data = clean_data(raw_history, symbols)
 signals = evaluate(history_data)
