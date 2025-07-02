51413bd1f79152c591b6020c5840543dc719cbf6
Brent Yee <brentyee28@gmail.com>
2025-07-02 10:32:03 +0800

    random strategy

--- strategy.py
+++ strategy.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import ta
 from typing import Dict, Any
+import random
 
 #PLANS: use multiple strategies, which strategy used: determine by ranking
 def evaluate(history_data, df):
@@ -14,7 +15,7 @@ def evaluate(history_data, df):
         #placeholder strategy
         signal = {
             "action": "BUY" if df["close"].iloc[-1] > df["close"].mean() else "HOLD",
-            "confidence": 0.85 #placeholder confidence
+            "confidence": random.random() #placeholder confidence
         }
         signals[symbol] = signal
     return signals
