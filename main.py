ba56571e9fdb696ffb1b715b7c20bfb3313c208b
Brent Yee <brentyee28@gmail.com>
2025-07-10 17:09:11 +0800

    added rank signals

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 from data import fetch_historical_data, fetch_live_data
 from strategy import evaluate
+from rank import rank
 
 load_dotenv()
 api_key = os.getenv("APCA_API_KEY_ID")
@@ -19,6 +20,10 @@ symbols = ["AAPL", "MSFT", "TSLA", "GOOG", "RKLB"] #symbols - trades these stock
 
 historical_data = fetch_historical_data(dataclient, symbols)
 live_data = fetch_live_data(liveclient, symbols)
+print(live_data)
 
 signals = evaluate(historical_data) #evaluate based on signals
 print(signals)
+
+ranked_signals = rank(signals, top_n = 3, conf_threshold = 0.5)
+print(ranked_signals)
