f16072e384273731c643bdebe2604aa8246e7e37
Brent Yee <brentyee28@gmail.com>
2025-07-10 17:04:56 +0800

    created rank.py

---
+++ rank.py
@@ -0,0 +1,14 @@
+def rank(signals, top_n, conf_threshold):
+    #remove holds
+    filtered_signals = {
+        symbol: signal for symbol, signal in signals.items()
+        if signal["action"] == "BUY" and signal["confidence"] >= conf_threshold 
+    }
+
+    ranked = sorted( #inbuilt "sorted" function
+        filtered_signals.items(),
+        key = lambda x: x[1]["confidence"], #sort by confidence
+        reverse = True #descending order
+    )
+
+    return(ranked[:top_n])
