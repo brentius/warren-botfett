6ebb492eab593f91a3b1c021262b7f5c0682a5f3
Brent Yee <brentyee28@gmail.com>
2025-07-10 17:15:15 +0800

    added rank_sell

--- rank.py
+++ rank.py
@@ -1,8 +1,23 @@
-def rank(signals, top_n, conf_threshold):
+def rank_buy(signals, top_n, conf_threshold):
     #remove holds
     filtered_signals = {
         symbol: signal for symbol, signal in signals.items()
         if signal["action"] == "BUY" and signal["confidence"] >= conf_threshold 
+    }
+
+    ranked = sorted( #inbuilt "sorted" function
+        filtered_signals.items(),
+        key = lambda x: x[1]["confidence"], #sort by confidence
+        reverse = True #descending order
+    )
+
+    return(ranked[:top_n])
+
+def rank_sell(signals, top_n, conf_threshold):
+    #remove holds
+    filtered_signals = {
+        symbol: signal for symbol, signal in signals.items()
+        if signal["action"] == "SELL" and signal["confidence"] >= conf_threshold 
     }
 
     ranked = sorted( #inbuilt "sorted" function
