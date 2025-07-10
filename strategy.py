b258a35d40363981156b8b4ad92e4e56023c8f8d
Brent Yee <brentyee28@gmail.com>
2025-07-10 16:41:18 +0800

    added placeholder strategy

---
+++ strategy.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import ta
+import random
+
+#PLANS: use multiple strategies, which strategy used: determine by ranking
+def evaluate(historical_data):
+    signals = {}
+    for symbol, df in historical_data.items():
+        #placeholder strategy
+        signal = {
+            "action": "BUY" if df["close"].iloc[-1] > df["close"].mean() else "HOLD",
+            "confidence": round(random.random(), 2) #placeholder confidence
+        }
+        signals[symbol] = signal
+    return signals
