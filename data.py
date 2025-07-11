908fb6fa82fa90e62d22745e108afa9b18b6d8aa
Brent Yee <brentyee28@gmail.com>
2025-07-11 17:18:04 +0800

    changed variable names

--- data.py
+++ data.py
@@ -2,13 +2,13 @@ import pandas as pd
 from alpaca.data.timeframe import TimeFrame
 from alpaca.data.requests import StockBarsRequest
 
-def fetch_historical_data(dataclient, symbols):
+def fetch_historical_data(client, symbols):
     request = StockBarsRequest(
         symbol_or_symbols = symbols,
         timeframe = TimeFrame.Day,
         start = "2025-01-01"
     )
-    raw_historical_data = dataclient.get_stock_bars(request).df
+    raw_historical_data = client.get_stock_bars(request).df
     def clean_data(raw_historical_data, symbols):
         history_data = {}
         for symbol in symbols:
@@ -22,11 +22,11 @@ def fetch_historical_data(dataclient, symbols):
         return history_data
     return clean_data(raw_historical_data, symbols)
 
-def fetch_live_data(liveclient, symbols):
+def fetch_live_data(client, symbols):
     async def quote_handler(data):
         print(data)
     live_data = {}
     for symbol in symbols:
-        data = liveclient.subscribe_quotes(quote_handler, symbol)
+        data = client.subscribe_quotes(quote_handler, symbol)
         live_data.update({symbol: data})
     return live_data
