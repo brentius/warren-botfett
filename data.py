8c8a216ae644a3a74d75f2206b2f6414a3a4c09a
Brent Yee <brentyee28@gmail.com>
2025-07-18 18:45:37 +0800

    removed unncessary function

--- data.py
+++ data.py
@@ -4,24 +4,22 @@ from alpaca.data.requests import StockBarsRequest
 from alpaca.data.requests import StockLatestQuoteRequest
 
 def fetch_historical_data(client, symbols):
+    history_data = {}
     request = StockBarsRequest(
         symbol_or_symbols = symbols,
         timeframe = TimeFrame.Day,
         start = "2025-01-01"
     )
     raw_historical_data = client.get_stock_bars(request).df
-    def clean_data(raw_historical_data, symbols):
-        history_data = {}
-        for symbol in symbols:
-            df = raw_historical_data.loc[symbol].copy()
-            df.index = pd.to_datetime(df.index)
-#           df.index = df.index.tz_localize("UTC").tz_convert("America/New_York")
-            df.sort_index(inplace = True)
-            df.columns = [col.lower() for col in df.columns]
-            df.dropna(subset=['close'], inplace=True)
-            history_data[symbol] = df
-        return history_data
-    return clean_data(raw_historical_data, symbols)
+    for symbol in symbols:
+        df = raw_historical_data.loc[symbol].copy()
+        df.index = pd.to_datetime(df.index)
+#       df.index = df.index.tz_localize("UTC").tz_convert("America/New_York")
+        df.sort_index(inplace = True)
+        df.columns = [col.lower() for col in df.columns]
+        df.dropna(subset=['close'], inplace=True)
+        history_data[symbol] = df
+    return history_data
 
 def fetch_live_data(client, symbols):
     live_prices = []
