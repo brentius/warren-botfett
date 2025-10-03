6c1095c985d6605b43f2a3042819b1f25a3d9c4e
brentius <brentyee28@gmail.com>
2025-10-03 18:32:26 +0100

    save

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 import yfinance as yf
 import pandas as pd
+import matplotlib
 
 ftse = yf.Ticker("^FTSE")
-
 data = yf.download("^FTSE", start="2015-01-01", end="2025-01-01")
 
-print(data.head())
+for 
